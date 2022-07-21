adc4109f27eaa856c867647266ea141cb64f8241
eshalali <eshal.a.ali@gmail.com>
2022-07-20 23:29:47 -0500

    add checkboxes to goals

--- controller/actual_routes.js
+++ controller/actual_routes.js
@@ -34,61 +34,6 @@ router.post('/', (req, res) => {
 // localhost:4000/actual/compare
 // only want to ever show the items for logged in user, so find by user ID
 router.get('/compare', (req, res) => {
-    // let expHousing = 0
-    // let expUtilities = 0
-    // let expInsurance = 0
-    // let expFood = 0
-    // let expTransport = 0
-    // let expMedical = 0
-    // let expPersonal = 0
-    // let expShopping = 0
-    // let expEnter = 0
-    // let expDebt = 0
-    // let expSavings = 0
-    // let expOther = 0
-    // Expected.find({ owner: req.session.userId })
-    //     .then( expected => {
-    //         if (expected[i].category === 'Housing') {
-    //             return expHousing+=expected[i].amount
-    //         } else if (expected[i].category === 'Utilities'){
-    //             return expUtilities+=expected[i].amount
-    //         } else if (expected[i].category === 'Insurance'){
-    //             return expInsurance+=expected[i].amount
-    //         } else if (expected[i].category === 'Food'){
-    //             return expFood+=expected[i].amount
-    //         } else if (expected[i].category === 'Transportation'){
-    //             return expTransport+=expected[i].amount
-    //         } else if (expected[i].category === 'Medical'){
-    //             return expMedical+=expected[i].amount
-    //         } else if (expected[i].category === 'Personal'){
-    //             return expPersonal+=expected[i].amount
-    //         } else if (expected[i].category === 'Shopping'){
-    //             return expShopping+=expected[i].amount
-    //         } else if (expected[i].category === 'Entertainment'){
-    //             return expEnter+=expected[i].amount
-    //         } else if (expected[i].category === 'Debt'){
-    //             return expDebt+=expected[i].amount
-    //         } else if (expected[i].category === 'Savings'){
-    //             return expSavings+=expected[i].amount
-    //         } else {
-    //             return expOther+=expected[i].amount
-    //         }
-    //     })
-    //     .catch(err => {
-    //         res.json(err)
-    //     })
-    let actHousing = 0
-    let actUtilities = 0
-    let actInsurance = 0
-    let actFood = 0
-    let actTransport = 0
-    let actMedical = 0
-    let actPersonal = 0
-    let actShopping = 0
-    let actEnter = 0
-    let actDebt = 0
-    let actSavings = 0
-    let actOther = 0
     Actual.find({ owner: req.session.userId })
         .then(actual => {
              res.render('actual/compare', { actual })
