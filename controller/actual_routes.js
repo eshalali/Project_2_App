61e09fd368470419af381d28a284ab5595abb466
eshalali <eshal.a.ali@gmail.com>
2022-07-21 00:23:25 -0500

    Add market ticker to investment page

--- controller/actual_routes.js
+++ controller/actual_routes.js
@@ -2,7 +2,7 @@
 //////////////////////////////////////
 const express = require('express')
 const Actual = require('../models/actual')
-const Expected = require('../models/expected')
+const fetch = require('node-fetch')
 
 // create router
 //////////////////////////////////////
@@ -36,8 +36,26 @@ router.post('/', (req, res) => {
 router.get('/compare', (req, res) => {
     Actual.find({ owner: req.session.userId })
         .then(actual => {
-             res.render('actual/compare', { actual })
+            let incomeAmount = 10
+            let expenseAmount = 20
+            for (item in actual) {
+                if (item.type === 'Income') {
+                    incomeAmount+=item.amount
+                } else {
+                    expenseAmount+=item.amount
+                }
+            }
+            fetch(`https://quickchart.io/chart?c={type:'bar',data:{labels:[Income,Expected],datasets:[{label:'Amount in $',data:[${incomeAmount}, ${expenseAmount}]}]}}`)
+            .then((apiRes) => {
+                return apiRes.json()
             })
+                .then((jsonData) => {
+                    res.render('actual/compare', { incomeAmount:incomeAmount, expenseAmount:expenseAmount })
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+                })
         .catch(err => {
             res.json(err)
         })
